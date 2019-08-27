2c0ca4d3ab3630d14d883759c602b18982964306
Siva Rachakonda <shiva.rachakonda139@gmail.com>
2019-08-28 01:32:39 +0530

    Update greeting end-point

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -12,7 +12,7 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greetings/2")
+    @RequestMapping("/greetings/1")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
