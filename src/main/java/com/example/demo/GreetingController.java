8197e6566d35a420ad2fa616f655546b7ad89bc2
Siva Rachakonda <shiva.rachakonda139@gmail.com>
2019-08-28 01:19:37 +0530

    Update greeting end-point

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -12,9 +14,18 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @RequestMapping("/greetings/1")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
+
+    /*@RequestMapping("/greetings")
+    public List<Greeting> greetings(@RequestParam(value="name", defaultValue="World") String name) {
+        Greeting greeting = new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+        Greeting secondGreeting = new Greeting(counter.incrementAndGet(),
+                String.format(template, "World Again"));
+        return Arrays.asList(greeting, secondGreeting);
+    }*/
 }
